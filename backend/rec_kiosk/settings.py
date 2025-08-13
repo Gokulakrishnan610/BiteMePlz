41cc28fc78ede2eaddce46d1269bd322abd7137a
Akash1912-hub <mkakash1912@gmail.com>
2025-08-14 01:56:08 +0530

    Update settings.py

--- backend/rec_kiosk/settings.py
+++ backend/rec_kiosk/settings.py
@@ -138,11 +138,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1:3000",
     "https://kisokrec.onrender.com",
-<<<<<<< HEAD
-
-=======
     "https://rec-kiosk-1.onrender.com",
->>>>>>> 166fe03d9debea96dac2e13af1d3962f77f265f2
 ]
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https?://localhost:\d+$",
