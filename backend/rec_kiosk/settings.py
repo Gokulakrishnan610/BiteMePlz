422c24d7678e35b00f5c39ad5eda17d8eb811334
Akash1912-hub <mkakash1912@gmail.com>
2025-08-14 02:01:47 +0530

    Update settings.py

--- backend/rec_kiosk/settings.py
+++ backend/rec_kiosk/settings.py
@@ -79,7 +79,7 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/tmp', 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
