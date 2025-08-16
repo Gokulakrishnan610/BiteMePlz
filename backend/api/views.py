1a4be7054b001bc321ac89560d73f3c437c1bed8
Akash1912-hub <mkakash1912@gmail.com>
2025-08-17 02:52:22 +0530

    Fix double slash issue in media URLs

--- backend/api/views.py
+++ backend/api/views.py
@@ -43,12 +43,16 @@ def serve_media_file(request, path):
             for item in os.listdir(file_path):
                 item_path = os.path.join(file_path, item)
                 if os.path.isfile(item_path):
-                    # Clean up path to avoid double slashes
+                    # Clean up path to avoid double slashes and handle empty paths
                     clean_path = path.rstrip('/') if path else ''
+                    if clean_path:
+                        url = f'/media/{clean_path}/{item}'
+                    else:
+                        url = f'/media/{item}'
                     files.append({
                         'name': item,
                         'size': os.path.getsize(item_path),
-                        'url': f'/media/{clean_path}/{item}' if clean_path else f'/media/{item}'
+                        'url': url
                     })
             
             # Return JSON response for directory listing
