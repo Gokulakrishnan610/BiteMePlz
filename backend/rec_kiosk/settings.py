8f38b366cb2b09d76d720d8422de12962784347e
SivaSabariGanesan <asivasabariganesan@gmail.com>
2025-08-17 15:45:23 +0530

    fix:redisdeploy

--- backend/rec_kiosk/settings.py
+++ backend/rec_kiosk/settings.py
@@ -82,8 +82,7 @@ try:
                 'default': {
                     'BACKEND': 'channels_redis.core.RedisChannelLayer',
                     'CONFIG': {
-                        'hosts': [config('REDIS_URL', default='rediss://default:ARv5AAImcDFkNGUyYzI4OTMxMjc0YjhkOTk0ZGU3N2ZkMjI1Y2Y2YXAxNzE2MQ@immortal-mutt-7161.upstash.io:6379')],
-                    },
+                    'hosts': [config('REDIS_URL', default='rediss://default:ARv5AAImcDFkNGUyYzI4OTMxMjc0YjhkOTk0ZGU3N2ZkMjI1Y2Y2YXAxNzE2MQ@immortal-mutt-7161.upstash.io:6379')],                    },
                 },
             }
             print("DEBUG: Using Redis channel layer for production")
